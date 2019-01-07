8ab3e3dd0cc91071106b8a977b49d12675243565
Imanol Ramajo <imanol.ramajo@gmail.com>
2019-01-07 19:20:41 +0100

    Renamed to EasyBooking_Client

---
+++ EasyBooking_Client/src/es/deusto/sd/easybooking/controller/Controller.java
@@ -0,0 +1,30 @@
+package es.deusto.sd.easybooking.controller;
+
+public class Controller {
+	
+
+	public Controller() {
+		
+	}
+	
+	public void search(Airport orig, Airport dest, String date) {
+		
+	}
+	
+	public boolean signUp() {
+		
+	}
+	
+	public boolean logIn() {
+		
+	}
+	
+	public reservationDTO getReservations(User user) {
+		
+	}
+	
+	public AirportDTO getAirportList() {
+		
+	}
+
+}
