287f919b38e2429112e46849da92ad2a4ed835c4
Imanol Ramajo <imanol.ramajo@gmail.com>
2019-01-08 20:57:22 +0100

    Renamed IRemote to ILufthansaServer

--- EB_Lufthansa/src/es/deusto/sd/lufthansa/ILufthansaServer.java
+++ EB_Lufthansa/src/es/deusto/sd/lufthansa/ILufthansaServer.java
@@ -7,7 +7,7 @@ import java.util.List;
 import es.deusto.sd.easybooking.dto.AirportDTO;
 import es.deusto.sd.easybooking.dto.FlightDTO;
 
-public interface IRemote extends Remote {
+public interface ILufthansaServer extends Remote {
  	public List<FlightDTO> searchFlights(AirportDTO origin, AirportDTO destination, Date date);
 	public void confirmReservation(); // @Todo
 	public FlightDTO getFlightData(int flightNumber);
