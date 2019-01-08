8ab63f94bdd08f540dcac96ae8ecd4ba5a0fb72a
Imanol Ramajo <imanol.ramajo@gmail.com>
2019-01-08 18:57:38 +0100

    VuelingGateway getFlightData() done

--- EasyBooking_Server/src/es/deusto/sd/easybooking/airline/VuelingGateway.java
+++ EasyBooking_Server/src/es/deusto/sd/easybooking/airline/VuelingGateway.java
@@ -79,13 +79,26 @@ public class VuelingGateway implements AirlineService {
 			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 		
 			out.writeUTF("get");
+			out.writeInt(flightNumber);
 			
-			// @Todo
+			// @Todo: If the airline does not have any flight with the given number it will
+			// return some error code, NOT IMPLEMENTED yet, so we will need to check if what
+			// we read from the read from the socket is a valid object.
+			// We could make the VuelingServer return a boolean + the FlightDTO, if the boolean is true
+			// it will be followed by the DTO else there is no DTO and the flightNumber does not match any flight
+			// However, there might be the possiblity that we always pass a valid flightNumber, 
+			// it depends on how we write our program
 			
+			FlightDTO f = (FlightDTO) in.readObject();
+			return f;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} // It will be more efficient to use a numeric constant here
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
